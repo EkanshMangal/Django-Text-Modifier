93ab58cf4a5c5b27237cded3367f827ff725a243
Ekansh MAngal <ekanshmangal102@gmail.com>
2020-05-08 00:45:54 +0530

    Laying Pipeline

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -2,8 +2,22 @@
 
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("hello ekansh How are you???")
 
-def about(request):
-    return HttpResponse("<h1> About me </h1>")      
+def index(request):
+    return HttpResponse("Home")
+
+def removepunc(request):
+    return HttpResponse("remove punc")
+
+def capfirst(request):
+    return HttpResponse("capitalize first")
+
+def newlineremove(request):
+    return HttpResponse("capitalize first")
+
+
+def spaceremove(request):
+    return HttpResponse("space remover")
+
+def charcount(request):
+    return HttpResponse("charcount ")
