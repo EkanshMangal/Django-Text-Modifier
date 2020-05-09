9ce410043ae5c630dabce46941dc659571a05464
Ekansh MAngal <ekanshmangal102@gmail.com>
2020-05-10 00:38:21 +0530

    analyze files are added

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,27 +1,44 @@
-# This file is made by me
-
+# I have created this file - Ekansh
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return render(request,'index.html')
 
-def removepunc(request):
+def index(request):
+    return render(request, 'index.html')
+
+    # return HttpResponse("Home")
+def analyze(request):
     #Get the text
     djtext = request.GET.get('text', 'default')
-    print(djtext)
-    #Analyze the text
-    return HttpResponse("remove punc")
 
-def capfirst(request):
-    return HttpResponse("capitalize first")
+    # Check checkbox values
+    removepunc = request.GET.get('removepunc', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremover = request.GET.get('newlineremover', 'off')
+    extraspaceremover = request.GET.get('extraspaceremover', 'off')
 
-def newlineremove(request):
-    return HttpResponse("capitalize first")
+    #Check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
 
+    else:
+        return HttpResponse("Error")
 
-def spaceremove(request):
-    return HttpResponse("space remover")
-
-def charcount(request):
-    return HttpResponse("charcount ")
+# def capfirst(request):
+#     return HttpResponse("capitalize first")
+#
+# def newlineremove(request):
+#     return HttpResponse("newline remove first")
+#
+#
+# def spaceremove(request):
+#     return HttpResponse("space remover back")
+#
+# def charcount(request):
+#     return HttpResponse("charcount ")
